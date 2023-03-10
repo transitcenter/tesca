91b4febe5fdd1407b5a284fef0b9eac29937e5cc
Willem Klumpenhouwer <willemjjk@gmail.com>
2023-03-10 09:21:35 -0500

    Set up docs.

---
+++ docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "tesca"
+copyright = "2023, TransitCenter"
+author = "TransitCenter"
+release = "0.0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosectionlabel"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {"navigation_depth": 4, "collapse_navigation": False}
+html_static_path = ["_static"]
+
+# add tesca package directory to path
+import pathlib
+import sys
+
+sys.path.insert(0, str(pathlib.Path().absolute().parent))
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. tesca documentation master file, created by
+   sphinx-quickstart on Fri Mar 10 09:11:49 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+TESCA: TransitCenter Equity Scenario Comparison Application
+===========================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
